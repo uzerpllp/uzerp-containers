1fd25f7ac5f3cd17a14b4bbdb8acc8d066d4e6fd
Steve Blamey <steve.blamey@adbury.net>
2022-05-20 14:37:49 +0100

    Upgrade to frepple 6.22.0 and rebase container on Ubuntu 20.04

--- uzerp-frepple/build.sh
+++ uzerp-frepple/build.sh
@@ -2,7 +2,7 @@
 set -o errexit
 
 # frepple version to build into the container image
-FREPPLE_VER="6.12.0"
+FREPPLE_VER="6.22.0"
 
 # Build the image using buildah
 buildah bud --build-arg FREPPLE_VERSION=$FREPPLE_VER -t quay.io/uzerp/uzerp-frepple:latest -t quay.io/uzerp/uzerp-frepple:$FREPPLE_VER
